8f165e4cf66206b85702f3fd1875a5ebacbbecb8
Erica Lederman <erical55@hotmail.com>
2012-10-26 22:24:50 -0400

    Initial Commit

---
+++ Nov1/Nov1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Nov1AppDelegate.h
+//  Nov1
+//
+//  Created by Erica Lederman on 10/26/12.
+//  Copyright (c) 2012 Erica Lederman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Nov1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Nov1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nov1
+//
+//  Created by Erica Lederman on 10/26/12.
+//  Copyright (c) 2012 Erica Lederman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Nov1AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([Nov1AppDelegate class]));
+	}
+}
